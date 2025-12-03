16225267b9abfd3ab07535b5e821bd4e2ec125f0
Albert <iavl@proton.me>
2025-12-03 19:10:14 +0800

    Refactor GasCost contract to use custom errors for validation; update tests for gas consumption comparisons and improve logging. Mark forge-std submodule as dirty.

--- src/GasCost.sol
+++ src/GasCost.sol
@@ -4,30 +4,23 @@ pragma solidity 0.8.26;
 contract GasCost {
     uint256 public number;
 
-    // Using require statement
-    function setNumberWithRequire(uint256 newNumber) public {
-        require(newNumber > 0, "Number must be greater than 0");
-        require(newNumber < 1000, "Number must be less than 1000");
-        number = newNumber;
-    }
-
-    // Using if revert statement
-    function setNumberWithIfRevert(uint256 newNumber) public {
-        if (newNumber == 0) {
-            revert("Number must be greater than 0");
-        }
-        if (newNumber >= 1000) {
-            revert("Number must be less than 1000");
-        }
-        number = newNumber;
-    }
-
     // Custom error definitions
     error NumberTooSmall();
     error NumberTooLarge();
 
-    // Using if revert with custom error (more gas efficient)
-    function setNumberWithIfRevertCustomError(uint256 newNumber) public {
+    // Using if revert with custom error (simulating require behavior)
+    function setNumberWithRequire(uint256 newNumber) public {
+        if (newNumber == 0) {
+            revert NumberTooSmall();
+        }
+        if (newNumber >= 1000) {
+            revert NumberTooLarge();
+        }
+        number = newNumber;
+    }
+
+    // Using if revert with custom error
+    function setNumberWithIfRevert(uint256 newNumber) public {
         if (newNumber == 0) {
             revert NumberTooSmall();
         }
--- test/GasCost.t.sol
+++ test/GasCost.t.sol
@@ -11,62 +11,8 @@ contract GasCostTest is Test {
         gasCost = new GasCost();
     }
 
-    // Test gas consumption of require (success case)
-    function test_SetNumberWithRequire_Success() public {
-        uint256 gasBefore = gasleft();
-        gasCost.setNumberWithRequire(100);
-        uint256 gasUsed = gasBefore - gasleft();
-        console.log("Gas used (require, success):", gasUsed);
-        assertEq(gasCost.number(), 100);
-    }
-
-    // Test gas consumption of if revert (success case)
-    function test_SetNumberWithIfRevert_Success() public {
-        uint256 gasBefore = gasleft();
-        gasCost.setNumberWithIfRevert(100);
-        uint256 gasUsed = gasBefore - gasleft();
-        console.log("Gas used (if revert, success):", gasUsed);
-        assertEq(gasCost.number(), 100);
-    }
-
-    // Test gas consumption of require (fail case - first condition)
-    function test_SetNumberWithRequire_FailFirst() public {
-        uint256 gasBefore = gasleft();
-        vm.expectRevert("Number must be greater than 0");
-        gasCost.setNumberWithRequire(0);
-        uint256 gasUsed = gasBefore - gasleft();
-        console.log("Gas used (require, fail first):", gasUsed);
-    }
-
-    // Test gas consumption of if revert (fail case - first condition)
-    function test_SetNumberWithIfRevert_FailFirst() public {
-        uint256 gasBefore = gasleft();
-        vm.expectRevert("Number must be greater than 0");
-        gasCost.setNumberWithIfRevert(0);
-        uint256 gasUsed = gasBefore - gasleft();
-        console.log("Gas used (if revert, fail first):", gasUsed);
-    }
-
-    // Test gas consumption of require (fail case - second condition)
-    function test_SetNumberWithRequire_FailSecond() public {
-        uint256 gasBefore = gasleft();
-        vm.expectRevert("Number must be less than 1000");
-        gasCost.setNumberWithRequire(1000);
-        uint256 gasUsed = gasBefore - gasleft();
-        console.log("Gas used (require, fail second):", gasUsed);
-    }
-
-    // Test gas consumption of if revert (fail case - second condition)
-    function test_SetNumberWithIfRevert_FailSecond() public {
-        uint256 gasBefore = gasleft();
-        vm.expectRevert("Number must be less than 1000");
-        gasCost.setNumberWithIfRevert(1000);
-        uint256 gasUsed = gasBefore - gasleft();
-        console.log("Gas used (if revert, fail second):", gasUsed);
-    }
-
     // Comparison test: gas consumption in success case
-    function test_CompareGasCost_Success() public {
+    function testCompareSuccess() public {
         // Test require
         gasCost.reset();
         uint256 gasBefore1 = gasleft();
@@ -88,21 +34,21 @@ contract GasCostTest is Test {
         );
     }
 
-    // Comparison test: gas consumption in fail case (first condition)
-    function test_CompareGasCost_FailFirst() public {
+    // Comparison test: gas consumption in fail case (number too small)
+    function testCompareFailSmall() public {
         // Test require
         uint256 gasBefore1 = gasleft();
-        vm.expectRevert("Number must be greater than 0");
+        vm.expectRevert(GasCost.NumberTooSmall.selector);
         gasCost.setNumberWithRequire(0);
         uint256 gasUsedRequire = gasBefore1 - gasleft();
 
         // Test if revert
         uint256 gasBefore2 = gasleft();
-        vm.expectRevert("Number must be greater than 0");
+        vm.expectRevert(GasCost.NumberTooSmall.selector);
         gasCost.setNumberWithIfRevert(0);
         uint256 gasUsedIfRevert = gasBefore2 - gasleft();
 
-        console.log("=== Gas Comparison (Fail First Condition) ===");
+        console.log("=== Gas Comparison (Fail - Number Too Small) ===");
         console.log("Require gas used:", gasUsedRequire);
         console.log("If revert gas used:", gasUsedIfRevert);
         console.log(
@@ -111,76 +57,26 @@ contract GasCostTest is Test {
         );
     }
 
-    // Comparison test: gas consumption in fail case (second condition)
-    function test_CompareGasCost_FailSecond() public {
+    // Comparison test: gas consumption in fail case (number too large)
+    function testCompareFailLarge() public {
         // Test require
         uint256 gasBefore1 = gasleft();
-        vm.expectRevert("Number must be less than 1000");
+        vm.expectRevert(GasCost.NumberTooLarge.selector);
         gasCost.setNumberWithRequire(1000);
         uint256 gasUsedRequire = gasBefore1 - gasleft();
 
         // Test if revert
         uint256 gasBefore2 = gasleft();
-        vm.expectRevert("Number must be less than 1000");
+        vm.expectRevert(GasCost.NumberTooLarge.selector);
         gasCost.setNumberWithIfRevert(1000);
         uint256 gasUsedIfRevert = gasBefore2 - gasleft();
 
-        console.log("=== Gas Comparison (Fail Second Condition) ===");
+        console.log("=== Gas Comparison (Fail - Number Too Large) ===");
         console.log("Require gas used:", gasUsedRequire);
         console.log("If revert gas used:", gasUsedIfRevert);
         console.log(
             "Difference:",
             gasUsedRequire > gasUsedIfRevert ? gasUsedRequire - gasUsedIfRevert : gasUsedIfRevert - gasUsedRequire
-        );
-    }
-
-    // Comparison test: string error vs custom error (success case)
-    function test_CompareGasCost_StringVsCustomError_Success() public {
-        // Test require (string error)
-        gasCost.reset();
-        uint256 gasBefore1 = gasleft();
-        gasCost.setNumberWithRequire(100);
-        uint256 gasUsedRequire = gasBefore1 - gasleft();
-
-        // Test if revert (custom error)
-        gasCost.reset();
-        uint256 gasBefore2 = gasleft();
-        gasCost.setNumberWithIfRevertCustomError(100);
-        uint256 gasUsedCustomError = gasBefore2 - gasleft();
-
-        console.log("=== Gas Comparison (String Error vs Custom Error, Success) ===");
-        console.log("Require (string) gas used:", gasUsedRequire);
-        console.log("If revert (custom error) gas used:", gasUsedCustomError);
-        console.log(
-            "Difference:",
-            gasUsedRequire > gasUsedCustomError
-                ? gasUsedRequire - gasUsedCustomError
-                : gasUsedCustomError - gasUsedRequire
-        );
-    }
-
-    // Comparison test: string error vs custom error (fail case)
-    function test_CompareGasCost_StringVsCustomError_Fail() public {
-        // Test require (string error)
-        uint256 gasBefore1 = gasleft();
-        vm.expectRevert("Number must be greater than 0");
-        gasCost.setNumberWithRequire(0);
-        uint256 gasUsedRequire = gasBefore1 - gasleft();
-
-        // Test if revert (custom error)
-        uint256 gasBefore2 = gasleft();
-        vm.expectRevert(GasCost.NumberTooSmall.selector);
-        gasCost.setNumberWithIfRevertCustomError(0);
-        uint256 gasUsedCustomError = gasBefore2 - gasleft();
-
-        console.log("=== Gas Comparison (String Error vs Custom Error, Fail) ===");
-        console.log("Require (string) gas used:", gasUsedRequire);
-        console.log("If revert (custom error) gas used:", gasUsedCustomError);
-        console.log(
-            "Difference:",
-            gasUsedRequire > gasUsedCustomError
-                ? gasUsedRequire - gasUsedCustomError
-                : gasUsedCustomError - gasUsedRequire
         );
     }
 }
